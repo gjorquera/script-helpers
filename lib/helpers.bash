8ec2cac2dace32b34ff6e061274dd1aa54d4fc4d
Gaston Jorquera <gjorquera@gmail.com>
2017-03-09 20:14:05 -0800

    Project link in helper file
    Add a small description of what this file is along with a link to the
    GitHub repo for easy reference if someone finds this file in another
    project.

--- lib/helpers.bash
+++ lib/helpers.bash
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Script Helpers
+#
+# A helper file that can sourced into your project scripts to to DRY them up.
+# If you don't know what project scripts are, please read
+# [scripts-to-rule-them-all](https://github.com/github/scripts-to-rule-them-all).
+# More information: https://github.com/gjorquera/script-helpers
+
 title() { echo -e "\033[0;34m\n${1}\n\033[0m"; }
 
 ensure() {
